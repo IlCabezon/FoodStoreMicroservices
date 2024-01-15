4e3763b23c4e80e4423c68998ef51d5659e2775c
IlCabezon <dev.francisco.castro@gmail.com>
2024-01-15 20:46:43 -0300

    feature: jest config

---
+++ products/src/services/product-service.test.js
@@ -0,0 +1,9 @@
+// which service it is
+describe("ProductService", () => {
+  describe("CreateProduct", () => {
+    // which scenario we are testing
+    test("validate user inputs ", () => {});
+
+    test("validate response", () => {});
+  });
+});
---
+++ shopping/src/services/shopping-service.test.js
@@ -0,0 +1,9 @@
+// which service it is
+describe("ShoppingService", () => {
+  describe("PlaceOrder", () => {
+    // which scenario we are testing
+    test("validate user inputs ", () => {});
+
+    test("validate response", () => {});
+  });
+});
